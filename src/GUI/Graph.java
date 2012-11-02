b70c08a05aef60ae7361962f79778c07ecdbfdac
Dei.Electi.mymail@gmail.com <Dei.Electi.mymail@gmail.com@4f1a5e00-4110-3180-091e-4bae18a7b435>
2012-11-02 20:47:28 +0000

    Какой-то глюк. Заливаю Graph

--- src/GUI/Graph.java
+++ src/GUI/Graph.java
@@ -1,4 +1,4 @@
-package GUI;
+package gui;
 
 import java.awt.Point;
 
@@ -7,36 +7,69 @@ import java.awt.Point;
  * @author Karimov
  */
 public abstract class Graph {
+	private Point coord;
+	private int width;
+	private int height;
+	
+	/**
+	 * Пока будут только квадратные модели
+	 * @param coord верхняя левая точка
+	 * @param width ширина
+	 * @param height высота
+	 */
+	public Graph(Point coord, int width, int height){
+		this.coord = coord;
+		this.width = width;
+		this.height = height;
+	}
+	
 	/**
 	 *  вывод элемента на экран
 	 * @return 
 	 */
-	abstract void draw();
+	public abstract void draw();
 	
 	/**
 	 * проверка на то, находится ли точки внутри объекта
 	 * @param p - точка, по которой ищется объект
 	 * @return <b>true</b> если есть объект 
 	 */
-	boolean pointAt(Point p){
-		return false;
+	public boolean pointAt(Point p){
+		return (p.x >= coord.x && p.x <= coord.x + width) && (p.y >= coord.y && p.y <= coord.y + height);
 	} 
 	
 	/**
 	 * вызывается при коллизии с др. объектом
 	 * @param obj с каким объектом произошла коллизия
 	 */
-	abstract void onCollision(Graph obj); 
-	
+	abstract void onCollision(Graph obj);
+
 	/**
 	 * Точка, в которой данный элемент находится
-	 * @return
 	 */
-	Point getCoord(){
-		return null;
+	public Point getCoord() {
+		return coord;
 	}
+
+	public void setCoord(Point coord) {
+		this.coord = coord;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	} 
 	
-	void setCoord(Point p){
-		
-	}
+	
 }
