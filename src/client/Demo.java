cace093f85beb55bff2e38b17f4e324f38f166ba
Dei.Electi.mymail@gmail.com <Dei.Electi.mymail@gmail.com@4f1a5e00-4110-3180-091e-4bae18a7b435>
2012-11-06 20:54:56 +0000

    Demo. Snake run around himself

--- src/client/Demo.java
+++ src/client/Demo.java
@@ -26,55 +26,70 @@ public class Demo {
 		new Screen();
 		while (!Screen.instance.canDraw())
 			Thread.sleep(100);
-		Map<Graph> m = new Map<Graph>("battle");
-		Snake sn = new Snake();
-		Element el = new Element(PARTS.HEAD, new Point(10, 0), 10, 10, sn);
-		sn.addElements(el);
-		int i;
-		for (i = 2; i < 30; i++) {
-			el = new Element(PARTS.BODY, new Point(i * 10, 0), 10, 10, sn);
-			sn.addElements(el);
-		}
-		el = new Element(PARTS.TAIL, new Point(i * 10, 0), 10, 10, sn);
-		sn.addElements(el);
-		sn.moveTo(100, 100);
-		m.putSnake(sn);
-		m.drawAll();
-		int waitTime = 500;
-		while (true) {
-			for (i = 0; i < 10; i++) {
-				long timeold = System.currentTimeMillis();
-				ActionFactory.getUp().doAction(sn);
-				Screen.instance.repaint();
-				//m.drawAll();
-				long timenow = System.currentTimeMillis() - timeold;
-				if (waitTime - timenow > 0)
-					Thread.sleep(waitTime - timenow);
+		Thread th = new Thread() {
+			public void run() {
+				try {
+					Map<Graph> m = new Map<Graph>("battle");
+					Snake sn = new Snake();
+					Element el = new Element(PARTS.HEAD, new Point(10, 0), 10,
+							10, sn);
+					sn.addElements(el);
+					int i;
+					for (i = 2; i < 30; i++) {
+						el = new Element(PARTS.BODY, new Point(i * 10, 0), 10,
+								10, sn);
+						sn.addElements(el);
+					}
+					el = new Element(PARTS.TAIL, new Point(i * 10, 0), 10, 10,
+							sn);
+					sn.addElements(el);
+					sn.moveTo(100, 100);
+					m.putSnake(sn);
+					m.drawAll();
+					int waitTime = 500;
+					while (true) {
+						for (i = 0; i < 10; i++) {
+							long timeold = System.currentTimeMillis();
+							ActionFactory.getUp().doAction(sn);
+							Screen.instance.repaint();
+							// m.drawAll();
+							long timenow = System.currentTimeMillis() - timeold;
+							if (waitTime - timenow > 0)
+								Thread.sleep(waitTime - timenow);
+						}
+						for (i = 0; i < 10; i++) {
+							long timeold = System.currentTimeMillis();
+							ActionFactory.getRight().doAction(sn);
+							m.drawAll();
+							long timenow = System.currentTimeMillis() - timeold;
+							if (waitTime - timenow > 0)
+								Thread.sleep(waitTime - timenow);
+						}
+						for (i = 0; i < 10; i++) {
+							long timeold = System.currentTimeMillis();
+							ActionFactory.getDown().doAction(sn);
+							m.drawAll();
+							long timenow = System.currentTimeMillis() - timeold;
+							if (waitTime - timenow > 0)
+								Thread.sleep(waitTime - timenow);
+						}
+						for (i = 0; i < 10; i++) {
+							long timeold = System.currentTimeMillis();
+							ActionFactory.getLeft().doAction(sn);
+							m.drawAll();
+							long timenow = System.currentTimeMillis() - timeold;
+							if (waitTime - timenow > 0)
+								Thread.sleep(waitTime - timenow);
+						}
+					}
+				} catch (SnakeAlreadyInMapException | InterruptedException
+						| ObjectAlreadyAddedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 			}
-			for (i = 0; i < 10; i++) {
-				long timeold = System.currentTimeMillis();
-				ActionFactory.getRight().doAction(sn);
-				m.drawAll();
-				long timenow = System.currentTimeMillis() - timeold;
-				if (waitTime - timenow > 0)
-					Thread.sleep(waitTime - timenow);
-			}
-			for (i = 0; i < 10; i++) {
-				long timeold = System.currentTimeMillis();
-				ActionFactory.getDown().doAction(sn);
-				m.drawAll();
-				long timenow = System.currentTimeMillis() - timeold;
-				if (waitTime - timenow > 0)
-					Thread.sleep(waitTime - timenow);
-			}
-			for (i = 0; i < 10; i++) {
-				long timeold = System.currentTimeMillis();
-				ActionFactory.getLeft().doAction(sn);
-				m.drawAll();
-				long timenow = System.currentTimeMillis() - timeold;
-				if (waitTime - timenow > 0)
-					Thread.sleep(waitTime - timenow);
-			}
-		}
+		};
+		th.setDaemon(true);
+		th.start();
 	}
 }
